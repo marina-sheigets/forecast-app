c52e501946cc21eb584082979e950589a90e6980
marina-sheigets <sejmar1705@gmail.com>
2025-07-15 14:06:36 +0300

    feat: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import "./App.css";
+import ForecastApp from "./components/ForecastApp/ForecastApp";
+
+function App() {
+  return <ForecastApp />;
+}
+
+export default App;
---
+++ src/components/ForecastApp/ForecastApp.tsx
@@ -0,0 +1,11 @@
+const ForecastApp = () => {
+  return (
+    <div className="forecast-app">
+      {/* <SearchBar />
+        <WeatherInfo />
+        <CityHistory /> */}
+    </div>
+  );
+};
+
+export default ForecastApp;
