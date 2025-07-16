23b19b1911ff68f31965a5e51a25d358a6b43e4a
marina-sheigets <sejmar1705@gmail.com>
2025-07-16 11:56:30 +0300

    test: searh bar

--- src/components/SearchBar/SearchBar.test.tsx
+++ src/components/SearchBar/SearchBar.test.tsx
@@ -1,5 +1,5 @@
 import { render, screen, fireEvent } from "@testing-library/react";
-import '@testing-library/jest-dom';
+import "@testing-library/jest-dom";
 import SearchBar from "./SearchBar";
 
 jest.mock("./SearchBar.module.css", () => ({
@@ -7,20 +7,24 @@ jest.mock("./SearchBar.module.css", () => ({
   default: {
     searchBarContainer: "searchBarContainer",
     input: "input",
-    button: "button"
-  }
+    button: "button",
+  },
 }));
+
+const props = {
+  handleFetchWeather: jest.fn(),
+};
 
 describe("SearchBar Component", () => {
   test("renders input and button", () => {
-    render(<SearchBar />);
-    
+    render(<SearchBar {...props} />);
+
     expect(screen.getByPlaceholderText(/enter your city/i)).toBeInTheDocument();
     expect(screen.getByRole("button", { name: /search/i })).toBeInTheDocument();
   });
 
   test("does nothing when input is empty", () => {
-    render(<SearchBar />);
+    render(<SearchBar {...props} />);
     const button = screen.getByRole("button", { name: /search/i });
 
     const consoleSpy = jest.spyOn(console, "log").mockImplementation(() => {});
